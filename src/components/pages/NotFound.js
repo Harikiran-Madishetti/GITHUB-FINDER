08f565923a752f2b46d902d8f7082df0b8c495db
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2020-09-15 04:06:07 -0500

    Added Home and NotFound Page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <p className='lead'>This is not the web page you are looking for.</p>
+        </div>
+    )
+}
+export default NotFound
